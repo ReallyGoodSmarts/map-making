cec5b5aba5daca3eb9bf3032294e58ed61829868
John Keefe <john@reallygoodsmarts.nyc>
2021-03-16 17:14:39 -0400

    built state map converter

---
+++ scripts/make_albers_states.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# turns state file into a simplified geojson file
+
+# usage (from project root): 
+#  bash scripts/make_albers_states.sh
+
+# deflate original census file
+unzip -o original-geos/cb_2018_us_state_500k.zip -d tmp
+
+# reproject and simplify the file
+npx mapshaper -i tmp/cb_2018_us_state_500k.shp \
+    -proj albersusa \
+    -simplify resolution=1200 \
+    -o geos/us_states_albers.json
+    
+# clean up the tmp folder
+rm tmp/*.*
+
