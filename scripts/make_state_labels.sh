5bc338eb07fb914f0997e79c2691ec8690a0ed08
John Keefe <john@reallygoodsmarts.nyc>
2021-03-20 14:25:59 -0400

    made state label svgs

--- scripts/make_state_labels.sh
+++ scripts/make_state_labels.sh
@@ -29,3 +29,30 @@ npx mapshaper -i geos/us_states_albers.json \
     -style class="g-state-name" \
     -each 'ID_BUILDER=`g-state-${STUSPS}`' \
     -o id-field=ID_BUILDER geos/us_states_albers_labels_nyt.json
+
+### Make SVGs ###
+    
+# make a point layer, attach labels to them, also class and IDs
+npx mapshaper -i geos/us_states_albers.json \
+    -points inner \
+    -style label-text=STUSPS \
+    -style class="g-state-name" \
+    -each 'ID_BUILDER=`g-state-${STUSPS}`' \
+    -o id-field=ID_BUILDER svgs/us_states_albers_labels_postal.svg
+    
+# same, but with full state names
+npx mapshaper -i geos/us_states_albers.json \
+    -points inner \
+    -style label-text=NAME \
+    -style class="g-state-name" \
+    -each 'ID_BUILDER=`g-state-${STUSPS}`' \
+    -o id-field=ID_BUILDER svgs/us_states_albers_labels_fullnames.svg
+    
+# same, but with NYT state names
+npx mapshaper -i geos/us_states_albers.json \
+    -join data/us_states_metadata.csv keys=STUSPS,postal \
+    -points inner \
+    -style label-text=nyt_name \
+    -style class="g-state-name" \
+    -each 'ID_BUILDER=`g-state-${STUSPS}`' \
+    -o id-field=ID_BUILDER svgs/us_states_albers_labels_nyt.svg
